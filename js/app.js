c384b44efaee3993b23736108b0c92d1ad338bc6
flor condori <condorihuamanif@gmail.com>
2017-03-02 10:13:23 -0500

    funcion calcularpeso primeros4planetas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-app
+function calcularPeso(){
+	var peso = parseFloat(document.getElementById('peso').value);
+	var planeta =document.getElementById('planetas').;
+	var resultado;
+
+ 
+	switch(planeta){
+	case("mercurio"): resultado = peso*2.78; break;
+	case("venus"): resultado = peso*8.87; break;
+	case("tierra"): resultado = peso*9.78; break;
+	case("marte"): resultado = peso*3.72; break;
+	}
+	
+ alert("Tu peso en "+planeta+" es: "+resultado.toFixed(2));
+}
